15fdc5212dde958bfb1308e498e64e4e0e4e8c51
jordancodurance <jordan.colgan@codurance.com>
2021-09-15 10:35:36 +0100

    Rotate right once for rover

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -1,2 +1,25 @@
 public class Rover {
+    private Direction direction = Direction.NORTH;
+    public String execute(String command) {
+        String nextCompass = direction.rightOf;
+        direction = Direction.valueOf(nextCompass);
+        return direction.compass;
+    }
+}
+
+enum Direction {
+    NORTH("N", "WEST", "EAST"),
+    EAST("E", "NORTH", "SOUTH"),
+    SOUTH("S", "EAST", "WEST"),
+    WEST("W", "SOUTH", "NORTH");
+
+    public String compass;
+    public String leftOf;
+    public String rightOf;
+
+    Direction(String compass, String leftOf, String rightOf) {
+        this.compass = compass;
+        this.leftOf = leftOf;
+        this.rightOf = rightOf;
+    }
 }
--- src/test/java/RoverShould.java
+++ src/test/java/RoverShould.java
@@ -1,2 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RoverShould {
+
+    private Rover target = new Rover();
+
+    @Test void
+    rotate_right() {
+        assertEquals("E", target.execute("R"));
+        assertEquals("S", target.execute("R"));
+        assertEquals("W", target.execute("R"));
+        assertEquals("N", target.execute("R"));
+    }
 }
